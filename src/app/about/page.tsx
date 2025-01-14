194bd241dfe28002fb241f553fdbf666250fcfba
Saschl <sascharudolf46@gmail.com>
2025-01-14 18:58:59 +0100

    fix: some typos

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <main className="flex min-h-screen items-center justify-center font-[family-name:var(--font-geist-sans)]">
       <div className="w-auto mx-auto px-5">
-        Hello there! This nothing special and only validates some IBAN numbers
+        Hello there! This is nothing special and only validates IBAN numbers
         and returns the bank name if possible. Have fun!
       </div>
     </main>
